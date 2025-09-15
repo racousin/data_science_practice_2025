2a780fe90a9823afdbb1793232b2408ceee31385
Dara Colleran <daracolleran1@gmail.com>
2025-09-15 23:11:19 +0200

    Exercise 1*: add mysupertools package with multiply()

---
+++ daracoll/module2/mysupertools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mysupertools',
+    version='0.1',
+    description='Simple package with a multiply function',
+    author='Dara Colleran',
+    author_email='you@example.com',
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
