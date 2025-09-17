4650038088cd5bfd857a23a1a1472957ed8da255
sweetlulubaby <liuxiaoke938@gmail.com>
2025-09-17 19:16:20 +0800

    Update exercise files

--- sweetlulubaby/module2/mysupertools/mysupertools/tool/operation_a_b.py
+++ sweetlulubaby/module2/mysupertools/mysupertools/tool/operation_a_b.py
@@ -2,4 +2,4 @@ def multiply(a, b):
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return a * b
     else:
-        raise ValueError("Ce ne sont pas des nombres")
+        return "error"
